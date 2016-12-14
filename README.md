295d4c339033d0400655267a037095c547963a2d
Ajin <zcjwhu@163.com>
2016-12-14 17:43:22 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-this is a project with spring、springMVC、mybatis
+This is a project with spring、springMVC、mybatis
+开发中遇到的问题和解决办法：
+更新地址：http://blog.csdn.net/whu_zcj
+(1)基于ubuntu下intel idea的maven ssm项目的搭建（其中包或和eclipse搭建中不同值得注意的地方）
+(2)使用springMVC来接收参数的几种办法
+(3)开发中遇到乱码的解决办法
+(4)创建单元测试用例（这一点也和eclipse操作不同）
---
+++ test/com/ajin/mapper/UserMapperTest.java
@@ -0,0 +1,46 @@
+package com.ajin.mapper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ajin on 16-12-14.
+ */
+public class UserMapperTest {
+    @Test
+    public void deleteByPrimaryKey() throws Exception {
+
+    }
+
+    @Test
+    public void insertUser() throws Exception {
+
+    }
+
+    @Test
+    public void insertSelective() throws Exception {
+
+    }
+
+    @Test
+    public void selectByPrimaryKey() throws Exception {
+
+    }
+
+    @Test
+    public void updateByPrimaryKeySelective() throws Exception {
+
+    }
+
+    @Test
+    public void updateByPrimaryKey() throws Exception {
+
+    }
+
+    @Test
+    public void selectUserByName() throws Exception {
+
+    }
+
+}
